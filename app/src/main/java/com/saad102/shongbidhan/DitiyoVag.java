633bfecd763dda16b3a39a693730051eec747f52
nazmussakibsaad102 <sakibsaad102@gmail.com>
2020-03-09 18:42:16 +0600

    First commit

---
+++ app/src/main/java/com/saad102/shongbidhan/DitiyoVag.java
@@ -0,0 +1,14 @@
+package com.saad102.shongbidhan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DitiyoVag extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ditiyo_vag);
+    }
+}
---
+++ app/src/main/java/com/saad102/shongbidhan/ui/home/HomeFragment.java
@@ -0,0 +1,83 @@
+package com.saad102.shongbidhan.ui.home;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.saad102.shongbidhan.DitiyoVag;
+import com.saad102.shongbidhan.Prostabona;
+import com.saad102.shongbidhan.ProthomVag;
+import com.saad102.shongbidhan.R;
+import com.saad102.shongbidhan.TritioVag;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        final Button bt1 = root.findViewById(R.id.bt1);
+        bt1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intetnt= new Intent(getActivity(), Prostabona.class);
+                       startActivity(intetnt);
+                    }
+                }
+        );
+
+
+        final Button bt2 = root.findViewById(R.id.bt2);
+        bt2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intetnt= new Intent(getActivity(), ProthomVag.class);
+                        startActivity(intetnt);
+                    }
+                }
+        );
+
+        final Button bt3 = root.findViewById(R.id.bt3);
+        bt3.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intetnt= new Intent(getActivity(), DitiyoVag.class);
+                        startActivity(intetnt);
+                    }
+                }
+        );
+        final Button bt4 = root.findViewById(R.id.bt4);
+        bt4.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intetnt= new Intent(getActivity(), TritioVag.class);
+                        startActivity(intetnt);
+                    }
+                }
+        );
+//        final TextView textView = root.findViewById(R.id.text_home);
+//        homeViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+        return root;
+    }
+}
